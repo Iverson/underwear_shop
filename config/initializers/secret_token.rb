3e74c34c4c8cf2d2f2e6273cfd12a008ade2020e
Iverson <iversonallen@yandex.ru>
2012-07-17 12:17:42 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnderwearShop::Application.config.secret_token = 'aa6d78e9534502358a1bb313c34edcd45c8d6ce2fe7296b54182120d195339122250ac8c413aed7ea2804dc59c2123c20d28006cfff111df47c0bb7eeb2fbf76'
