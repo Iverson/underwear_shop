3fc8e32fe76b18ba87f14137affc2fa75edb9386
Iverson <iversonallen@yandex.ru>
2012-11-07 21:32:51 +0400

    production config DB

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,7 @@ server "62.76.186.158", :app, :web, :db, :primary => true
 
 after 'deploy:update_code', :roles => :app do
   # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+  
 end
 
 # Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
