c959d36423048e5f2a8289ff52580b1da8613d56
Alexey Krasman <akrasman@171571-10001.vm.clodoserver.ru>
2012-11-07 17:59:40 +0000

    production config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-deploy_to  = "/srv/myapp"
+deploy_to  = "/home/akrasman/underwear_shop"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
