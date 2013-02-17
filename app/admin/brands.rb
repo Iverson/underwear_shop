58e55bf11baea7a2d39053ad57a468c3f678b10d
Iverson <akrasman@gmail.com>
2013-02-18 01:31:29 +0300

    Brands to_param fix for Active Admin

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -1,5 +1,23 @@
 ActiveAdmin.register Brand do
   menu :label => proc{ I18n.t("active_admin.brands") }, :parent => I18n.t("active_admin.products"), :priority => 3
   
+  actions :all, :except => [:show]
   
+  around_filter do |controller, action|
+    Brand.class_eval do
+      def to_param
+        id.to_s
+      end
+    end
+
+    begin
+      action.call
+    ensure
+      Brand.class_eval do
+        def to_param
+          "#{uri}"
+        end
+      end
+    end
+  end
 end
