d7271c061aa01a21118f0b59f110786ed9db7a91
Iverson <akrasman@gmail.com>
2014-02-15 17:36:07 +0300

    Active admin. Page model index view.

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Page do
   menu :priority => 100, :label => proc{ I18n.t("active_admin.pages") }
+  
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :uri
+    # column :description do |p|
+    #   truncate(p.description, :length => 100)
+    # end
+    default_actions
+  end
 end
