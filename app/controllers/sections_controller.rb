4fa48d9ba10352fc49d73136122608da96c0b8ff
Iverson <akrasman@gmail.com>
2014-06-08 13:14:39 +0400

    Fix catalog root section query.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -14,7 +14,16 @@ class SectionsController < ApplicationController
     @active_section_id = @section.id
     @active_parent_id  = @section.parent.try(:id)
     @seo_key = "мужские #{@section.name.mb_chars.downcase.to_s}"
-    @products = @section.products.puplished.includes(:pictures, :product_instances).top.order(params[:sort_by]).paginate(:page => params[:page], :per_page => @per_page)
+
+    if @section.has_children?
+      subsections_ids = @section.child_ids.push(@section.id)
+      @products = Product.where(section_id: subsections_ids)
+    else
+      @products = @section.products
+    end
+
+    @products = @products.puplished.includes(:pictures, :product_instances).top.order(params[:sort_by]).paginate(:page => params[:page], :per_page => @per_page)
+
     @title = "Купить мужские #{@section.name.mb_chars.downcase.to_s} с бесплатной доставкой в интернет магазине YoungLovers.ru"
     
     add_breadcrumb "<span>#{@section.name}</span>", section_url
