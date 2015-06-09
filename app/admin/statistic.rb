08b2db9d19ab6c669d97f15061c4acdc158d9bf2
Alexey Krasman <akrasman@gmail.com>
2015-06-09 11:43:21 +0300

    Fix statistics

--- app/admin/statistic.rb
+++ app/admin/statistic.rb
@@ -1,40 +1,40 @@
 # encoding: utf-8
 ActiveAdmin.register_page "Statistics" do
   menu :priority => 100, :label => proc{ I18n.t("active_admin.statistics") }
-  
+
   content do
     para "Статистика"
-    
+
     start_date = Order.order(:created_at).first.created_at
     end_date = Date.current
     show_all = false
-    
+
     if params[:from_date] && params[:to_date]
       from_date = Date.parse(params[:from_date])
-      to_date = Date.parse(params[:to_date])
+      to_date = Date.parse(params[:to_date]) + 1.day
     else
       from_date = Date.new(end_date.year, end_date.month)
       to_date = end_date
     end
-    
+
     orders = Order.where(:state => :paid, :created_at => from_date..to_date).order(:id)
-    
+
     years_html = ""
-    
+
     (start_date.year..end_date.year).each do |y|
       if y == from_date.year && y == to_date.year
         years_html += "<b>#{y}</b>"
       else
         years_html += link_to y, "?from_date=#{Date.new(y, 1).strftime("%Y-%m-%d")}&to_date=#{(Date.new(y, 1)+ 1.month).strftime("%Y-%m-%d")}"
       end
-    
+
       years_html += "&nbsp;&nbsp;&nbsp;"
     end
-    
+
     para years_html.html_safe
-    
+
     month_html = ""
-    
+
     (from_date.year..to_date.year).each do |y|
        mo_start = (start_date.year == y) ? start_date.month : 1
        mo_end = (end_date.year == y) ? end_date.month : 12
@@ -45,18 +45,18 @@ ActiveAdmin.register_page "Statistics" do
           else
             month_html += link_to Date::MONTHNAMES[m], "?from_date=#{Date.new(y, m).strftime("%Y-%m-%d")}&to_date=#{(Date.new(y, m)+ 1.month - 1.day).strftime("%Y-%m-%d")}"
           end
-           
+
            month_html += "&nbsp;&nbsp;&nbsp;"
        end
     end
-    
+
     para month_html.html_safe
-  
+
     if orders.empty?
-      
+
       para "<b>Нет заказов</b>".html_safe
     else
-      
+
       table_for orders do |f|
         f.column :id
         f.column "User" do |o|
@@ -70,7 +70,7 @@ ActiveAdmin.register_page "Statistics" do
         end
 
       end
-      
+
       total = orders.sum{|item| item.summ}
       purchaise_total = orders.joins(:order_items => :product).sum(:purchaise_price)
 
@@ -81,10 +81,10 @@ ActiveAdmin.register_page "Statistics" do
         para "<b>Прибыль:</b> #{total.to_i - purchaise_total.to_i}".html_safe
         para "<b>Средний заказ:</b> #{total.to_i / orders.count}".html_safe
       end
-      
+
     end
-    
-    
-    
+
+
+
   end
 end
